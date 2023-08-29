15a8248df2ac7f2130786baa85fc0d9d967d6423
Hikmat Hidayat <hikmat6@gmail.com>
2023-08-29 10:25:42 +0700

    fortify, email verify

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.blank-page', ['type_menu' => '', 'name' => 'home']);
+    // return view('welcome');
+    return view('auth.login', ['type_menu' => '']);
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.blank-page', ['type_menu' => '']);
+    })->name('home');
 });
